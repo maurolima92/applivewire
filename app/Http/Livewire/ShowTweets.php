d15a5c8597fd65005789dee4c6a213d2fba66e9c
Mauro Lima <mauroaugusto.92@gmail.com>
2022-02-04 00:16:06 -0300

    Jetstream adicionado

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -4,16 +4,33 @@ namespace App\Http\Livewire;
 
 use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
-class ShowTweets extends Component
-{
-    public $mensage = 'Apenas um teste';
+class ShowTweets extends Component{
+    use WithPagination;
+    public $content = 'Apenas um teste';
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
-        $tweets = Tweet::with('user')->get();
+        $tweets = Tweet::with('user')->paginate(2);
         return view('livewire.show-tweets', [
             'tweets' => $tweets
         ]);
+    }
+
+    public function create(){
+
+        $this->validate();
+
+        Tweet::create([
+            'content'=> $this->content,
+            'user_id' => 1,
+        ]);
+
+        $this->content = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,11 +1,23 @@
 <div>
     <h2>Show Tweets</h2>
 
-    <p>{{ $mensage }}</p>
+    <p>{{ $content }}</p>
 
-    <input type="text" id="mensage" name="mensage" wire:model="mensage">
+    <hr>
+    <form method="post" wire:submit.prevent="create">
+        <input type="text" id="content" name="content" wire:model="content">
+            @error('content')
+                {{ $message }}
+            @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
     <hr>
     @foreach ($tweets as $tweet)
        <p>{{$tweet->id}} - {{ $tweet->user->name}} - {{ $tweet->content }}</p>
     @endforeach
+
+    <hr>
+    <div class="paginacao">
+        {{ $tweets->links() }}
+    </div>
 </div>
